9133e8fc2347922f971e952446d42d8a25fcdc3e
user <user@DESKTOP-SOOO9EI>
2018-09-02 01:21:17 +0200

    Version 5.4
    Gestion de la datasource par spring

--- Game/game-dao/src/main/java/com/tutorial/game/dao/impl/AbstractDAO.java
+++ Game/game-dao/src/main/java/com/tutorial/game/dao/impl/AbstractDAO.java
@@ -5,17 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 public abstract class AbstractDAO {
-
-	final static String URL = "jdbc:mysql://localhost:3306/videogame?serverTimezone=UTC";
-	final static String USER = "root";
-	final static String PASSWORD = "gemar020688";
-	Connection connexion = null;
-
-	protected Connection getConnection() throws NamingException, SQLException {
-		connexion = DriverManager.getConnection(URL, USER, PASSWORD);
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	protected Connection getConnection() throws NamingException, SQLException {		
 		
-		return connexion;
+		return dataSource.getConnection();
 	}
 }
--- Game/game-webapp/src/main/java/com/tutorial/game/listener/GameInitialisationListener.java
+++ Game/game-webapp/src/main/java/com/tutorial/game/listener/GameInitialisationListener.java
@@ -14,13 +14,6 @@ public class GameInitialisationListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		LOG.info("Application Game Start...");
-		
-		/* Chargement du driver JDBC pour MySQL */
-		try {
-		    Class.forName( "com.mysql.cj.jdbc.Driver" );
-		} catch ( ClassNotFoundException e ) {
-		    /* Gérer les  éventuelles erreurs ici. */
-		}
 
 		LOG.info("Application Game Started !");
 	}
