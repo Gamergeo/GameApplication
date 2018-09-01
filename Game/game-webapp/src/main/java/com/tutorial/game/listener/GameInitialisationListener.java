8da8dda3ccbeed439a37a68a17ecfe4c38bc2b2b
user <user@DESKTOP-SOOO9EI>
2018-09-02 00:18:36 +0200

    Version 5.3.1
    Clean

--- Game/game-webapp/src/main/java/com/tutorial/game/listener/GameInitialisationListener.java
+++ Game/game-webapp/src/main/java/com/tutorial/game/listener/GameInitialisationListener.java
@@ -1,15 +1,19 @@
 package com.tutorial.game.listener;
 
+import java.util.logging.Logger;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
-public class GameExceptionListener implements ServletContextListener {
+public class GameInitialisationListener implements ServletContextListener {
+	
+	private static Logger LOG = Logger.getLogger(GameInitialisationListener.class.getName());
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		System.out.println("Application Game Start...");
+		LOG.info("Application Game Start...");
 		
 		/* Chargement du driver JDBC pour MySQL */
 		try {
@@ -17,14 +21,15 @@ public class GameExceptionListener implements ServletContextListener {
 		} catch ( ClassNotFoundException e ) {
 		    /* Gérer les  éventuelles erreurs ici. */
 		}
-		System.out.println("Application Game Started !");
+
+		LOG.info("Application Game Started !");
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		System.out.println("Application Game Closing...");
-		
-		System.out.println("Application Game Closed !");
+		LOG.info("Application Game Closing...");
+
+		LOG.info("Application Game Closed !");
 	}
 
 }
