b249bbca25affd2498d2f072d6e0d33cfe26fdec
user <user@DESKTOP-SOOO9EI>
2018-09-02 04:28:05 +0200

    Version 5.6

--- Game/game-dao/src/main/java/com/tutorial/game/dao/impl/AbstractDAO.java
+++ Game/game-dao/src/main/java/com/tutorial/game/dao/impl/AbstractDAO.java
@@ -1,15 +1,12 @@
 package com.tutorial.game.dao.impl;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.mysql.cj.jdbc.MysqlDataSource;
 
 public abstract class AbstractDAO {
 	
@@ -19,5 +16,9 @@ public abstract class AbstractDAO {
 	protected Connection getConnection() throws NamingException, SQLException {		
 		
 		return dataSource.getConnection();
+	}
+	
+	protected DataSource getDataSource() {
+		return dataSource;
 	}
 }
